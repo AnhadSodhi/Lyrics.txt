ff8a51b1925c1bf286efaaec87870187cfc7a2dd
Anhad Sodhi <anhadsodhi2005@gmail.com>
2025-03-29 20:07:01 -0700

    Cleaned up imports

--- app/src/main/java/com/example/lyricstxt/Home.kt
+++ app/src/main/java/com/example/lyricstxt/Home.kt
@@ -5,16 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
--- app/src/main/java/com/example/lyricstxt/MainActivity.kt
+++ app/src/main/java/com/example/lyricstxt/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import com.example.lyricstxt.api.ClientController
 import com.example.lyricstxt.data.HistoryRepository
 import com.example.lyricstxt.data.MyDatabase
