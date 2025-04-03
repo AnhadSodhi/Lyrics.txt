aae52efe145e0c59cfe9005508301bb199907473
Anhad Sodhi <anhadsodhi2005@gmail.com>
2025-04-02 17:29:08 -0700

    Moved ClientController functionality to HomeState

--- app/src/main/java/com/example/lyricstxt/home/HomeState.kt
+++ app/src/main/java/com/example/lyricstxt/home/HomeState.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.lyricstxt.api.Song
 import com.example.lyricstxt.api.getAccessToken
 import com.example.lyricstxt.api.getCurrentSong
@@ -17,31 +15,27 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.gson.gson
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class HomeState (
-    private val auth: String,
-    private val refresh: String
-) : ViewModel() {
-    private lateinit var basicClient: HttpClient
-    private lateinit var authClient: HttpClient
-
+class HomeState(private val auth: String, private val refresh: String, private val initScope: CoroutineScope) {
     var lyrics by mutableStateOf(listOf("Loading..."))
     var times by mutableStateOf(emptyList<Long>())
-    var currentLineIndex by mutableIntStateOf(0)
+    val currentLineIndex = mutableIntStateOf(0)
     var startTime by mutableLongStateOf(0)
     var song by mutableStateOf(Song("", "", ""))
 
+    private val basicClient = HttpClient {
+        install(ContentNegotiation) {
+            gson()
+        }
+    }
+    private var authClient: HttpClient? = null
+
     init {
-        viewModelScope.launch {
-            basicClient = HttpClient {
-                install(ContentNegotiation) {
-                    gson()
-                }
-            }
-
+        initScope.launch {
             val token = getAccessToken(basicClient, auth, refresh)
-
             authClient = basicClient.config {
                 defaultRequest {
                     header(HttpHeaders.Authorization, "Bearer $token")
@@ -51,16 +45,16 @@ class HomeState (
     }
 
     suspend fun getSongAndProgress(): Pair<Song, Long> {
+        val authClient = authClient ?: return Pair(Song("", "", ""), 0)
         return getCurrentSong(authClient)
     }
 
-    suspend fun updateSongAndProgress(offset: Int) {
+    suspend fun updateSongAndProgress(offset: Int=0) {
         try {
-            val (s, progress) = getCurrentSong(authClient)
+            val (s, p) = getSongAndProgress()
             song = s
-            startTime = System.currentTimeMillis() - (progress + offset)
-        }
-        catch (e: Exception) {
+            startTime = System.currentTimeMillis() - (p + offset)
+        } catch (e: Exception) {
             song = Song("", "", "")
         }
     }
